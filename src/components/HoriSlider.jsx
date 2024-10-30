da8ade63bc4e3c9dc028c6683b34c9b23727caa3
aayushkaksha <147289276+aayushkaksha@users.noreply.github.com>
2024-10-30 15:15:53 +0545

    horislider actually works

--- src/components/HoriSlider.jsx
+++ src/components/HoriSlider.jsx
@@ -1,17 +1,23 @@
-import HoriCard from './HoriCards';
-import horiCardData from '../dataFile/horiCardData.json'
+import HoriCard from "./HoriCards";
+import horiCardData from "../dataFile/horiCardData.json";
 
 const HorizontalSlider = () => {
   return (
-    <div className="overflow-x-auto flex whitespace-nowrap py-4">
-      <div className="flex gap-4">
-        {horiCardData.cards.map((card, index) => (
-          <HoriCard key={index} image={card.image} label={card.label} />
-        ))}
+    <div className="">
+      <p className="h3 text-b">Trending</p>
+      <div className="overflow-x-auto flex whitespace-nowrap py-4">
+        <div className="flex gap-4">
+          {horiCardData.map(() => (
+            <HoriCard
+              key={horiCardData.id}
+              image={horiCardData.image}
+              label={horiCardData.label}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
 };
-
 
 export default HorizontalSlider;
